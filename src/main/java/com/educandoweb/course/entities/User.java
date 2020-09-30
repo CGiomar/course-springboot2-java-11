26168a645ae935a26c271d3059cc5d3e4ebdaeeb
Carlos Giomar <ardethcgoetia@gmail.com>
2020-09-30 18:19:23 +0100

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,7 +22,7 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -35,8 +35,8 @@ public class User implements Serializable {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = (long) id;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -75,7 +75,7 @@ public class User implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = (int) (prime * result + id);
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -88,7 +88,10 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
